ffa1c9dc8c27dcdd8351652995120e7dc4602530
TaylorJi <sji24@my.bcit.ca>
2024-01-31 03:51:33 -0800

    completed personal web

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+        // const earth = document.getElementById('earth');
+        // const languages = document.querySelectorAll('.language');
+    
+        // const earthWidth = earth.offsetWidth; // 지구 이미지의 너비
+        // const earthHeight = earth.offsetHeight; // 지구 이미지의 높이
+        // const orbitRadius = 200; // 궤도의 반경 (픽셀)
+        // const speed = 0.0005; // 회전 속도
+    
+        // function updatePosition(el, angle, radius) {
+        //     const earthCenterX = earth.offsetLeft + earthWidth / 2;
+        //     const earthCenterY = earth.offsetTop + earthHeight / 2;
+    
+        //     const x = earthCenterX + radius * Math.cos(angle) - el.offsetWidth / 2;
+        //     const y = earthCenterY + radius * Math.sin(angle) - el.offsetHeight / 2;
+    
+        //     el.style.left = `${x}px`;
+        //     el.style.top = `${y}px`;
+        // }
+    
+        // function animate() {
+        //     languages.forEach((lang, index) => {
+        //         const angle = performance.now() * speed + index * (2 * Math.PI / languages.length);
+        //         updatePosition(lang, angle, orbitRadius);
+        //     });
+        //     requestAnimationFrame(animate);
+        // }
+    
+        // animate();
